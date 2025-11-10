b4cbde3ed6b7c2b3e9b7c57b56eca6cdfab7beed
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-11-10 16:31:20 +0200

    Completed Assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,31 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		
+		int terms = Integer.parseInt(args[0]);
+		double piApprox = 1;
+		double oddCount = 3;
+		
+		// Calculation of approx. pi / 4 (depending on amount of terms asked to check)
+		// 1 is the first term, then it turns into either +/- 1/odd
+		for (int i = 0; i < terms - 1; i++) {
+			if (i % 2 == 0) {
+
+				piApprox -= (1 / oddCount);
+				oddCount += 2;
+
+			} else {
+
+				piApprox += (1 / oddCount);
+				oddCount += 2;
+
+			}
+		}
+
+		// Prints actual pi (approx. according to Java)
+		System.out.println("pi according to Java: " + Math.PI);
+
+		// Prints pi approx. (according to Gregory and Leibnitz and the amount of terms asked to check)
+		System.out.println("pi, approximated:	 " + (4 * piApprox));
 	}
 }
