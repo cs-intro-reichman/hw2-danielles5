bf53468d9835145238d5442ded5f1a75edf3d9a9
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-11-11 15:47:28 +0200

    Completed Assignment

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,58 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+
+		int seedN = Integer.parseInt(args[0]);
+		String cOrV = args[1];	
+ 
+		if ("vV".indexOf(cOrV) != -1) {
+
+			for (int i = 1; i <= seedN; i++){
+
+				int seed = i;
+				int counter = 1;
+				System.out.print(seed + " ");
+
+				do {
+					if (seed % 2 == 0){
+						seed /= 2;
+						counter++;
+						System.out.print(seed + " ");
+					} else {
+						seed = (seed * 3) + 1;
+						counter++;
+						System.out.print(seed + " ");
+					}
+				} while (seed != 1);
+
+				System.out.println("(" + counter + ")" );
+				if (i == seedN && seed == 1) {
+					System.out.println("Every one of the first " + seedN + " hailstone sequences reached 1.");
+				}
+			}
+
+		} else if ("cC".indexOf(cOrV) != -1) {
+
+			for (int i = 1; i <= seedN; i++){
+
+				int seed = i;
+
+				do {
+					if (seed % 2 == 0){
+						seed /= 2;
+					} else {
+						seed = (seed * 3) + 1;
+					}
+				} while (seed != 1);
+
+				if (i == seedN && seed == 1) {
+					System.out.println("Every one of the first " + seedN + " hailstone sequences reached 1.");
+				}
+
+			}
+
+		}
+
+			
 	}
 }
